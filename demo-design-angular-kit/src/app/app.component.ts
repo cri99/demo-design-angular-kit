0d7d9f90e6bbdcddd1c364de39bf429420c194be
Your name here <critian.borelli@lascaux.it>
2022-11-15 22:41:54 +0100

    setup form base

--- demo-design-angular-kit/src/app/app.component.ts
+++ demo-design-angular-kit/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'demo-design-angular-kit';
+export class AppComponent implements OnInit {
+  
+  reactiveFormGroup!: FormGroup;
+  constructor(private _formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.reactiveFormGroup = this._formBuilder.group({
+      fullname: ['', Validators.required],
+      email: ['test+123@test.it', [Validators.required, Validators.email]],
+      age: [12, [Validators.min(10), Validators.max(100)]],
+      sex: ['M', Validators.required],
+      school: ['', Validators.required],
+      acceptPrivacyPolicy: [false, Validators.requiredTrue],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    })
+  }
+
+  onSubmit() {
+    console.log("Valore della form: ", this.reactiveFormGroup.value)
+  }
 }
--- demo-design-angular-kit/src/app/app.module.ts
+++ demo-design-angular-kit/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule
   ],
